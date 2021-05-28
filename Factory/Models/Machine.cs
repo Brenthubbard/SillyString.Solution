ec347b8ed474e2dd97b5b8179b6b5504da529b89
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-28 14:56:21 -0600

    migrations are working now working on machine create page

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -16,17 +16,8 @@ namespace Factory.Models
     [Required]
     public string Name { get; set; }
     [Required]
-
     public string MachineType { get; set; }
-    // [Required]
-    // public string Subject { get; set; }
-    // [Required]
     public int MachineNumber { get; set; }
-    // [Required]
-    // public float NumberOfCredits { get; set; }
     public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
-    // - (1) -- MachineId (1) -- student(1)
-    // - (2) -- MachineId (2) -- student(1)
-
   }
 }
